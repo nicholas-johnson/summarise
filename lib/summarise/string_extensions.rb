ff82fac8f4a5ec69bad1bd2da663b90a18e78f95
Nicholas Johnson <nicholas@forwardadvance.com>
2013-09-26 12:33:36 +0100

    adds the ability to append a suffix to summarised text.

--- lib/summarise/string_extensions.rb
+++ lib/summarise/string_extensions.rb
@@ -1,10 +1,14 @@
 
 module Summarise
   module StringExtensions
-    def summarise(l=200)
-      i = 0
-      self.split.map{ |word| word if (i += word.length) < l}.compact.join(' ')
+  def summarize(l=200, args={})
+    i = 0
+    summary = self.split.map{ |word| word if (i += word.length) < l}.compact.join(' ')
+    if args[:suffix] && self != summary
+      summary << args[:suffix]
     end
+    summary
+  end
 
     def summarisable?(length=200)
       return self.summarise(length) != self
